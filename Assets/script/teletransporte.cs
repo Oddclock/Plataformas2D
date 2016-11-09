374adc1a4b3fe8664067e14abddb107e0be1be4c
Oddclock <javiergarcia.escav@gmail.com>
2016-11-09 17:18:54 +0100

    Teletransporte
    Teletransporte del punto Origen al punto Destino

---
+++ Assets/script/teletransporte.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teletransporte : MonoBehaviour {
+
+	public Transform destino;
+
+	void OnTriggerEnter2D (Collider2D objeto){
+		if (objeto.tag == "Player") {
+			objeto.transform.position = destino.position;
+		
+		}
+	}
+}

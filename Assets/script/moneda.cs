2d8d0070f1a3bb046a88ff9bca4f29d0cbc33d15
Oddclock <javiergarcia.escav@gmail.com>
2016-11-09 16:46:47 +0100

    Subida inicial
    Aquí haremos la subida inicial

---
+++ Assets/script/moneda.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moneda : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("Alguien me ha tocado!");
+		Destroy (gameObject);
+	}
+}
